f7172ee70203bad31a76b2c5a49b6006dcb0cab6
Trimbak Pakhale <pakhaletrimbak@gmail.com>
2023-06-22 16:42:59 +0530

    hi

--- src/Components/SearchInput.js
+++ src/Components/SearchInput.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import axios from "axios";
 
 // api = https://api.unsplash.com/
@@ -9,13 +9,22 @@ const SearchInput = ({addImages}) =>{
 
 const [searchItem, setSearchItem] = useState("");
 
+// when someone open this sute fir the first time we have to show them a random images fot that we need useeffect and a function 
+
+// useeffect
+useEffect(()=>{
+    handleChnage();
+},[]);
+
+
 function handleChnage() {
     axios.get("https://api.unsplash.com/search/photos", {
         params:{
             client_id: "RfUnoLkMCK-DC6PcQd5qNtMDn_mhqmWDGPELAqXfU48",
             // query was foregt to add
-            query: searchItem
-        }
+            query: searchItem, 
+            count: 10,
+        },
     })
     .then(response=> addImages(response.data.results))
     .catch(err => console.log(err))
